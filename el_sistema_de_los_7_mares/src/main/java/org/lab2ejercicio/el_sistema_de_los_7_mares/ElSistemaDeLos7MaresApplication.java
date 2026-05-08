1f90ca51d46f11fc7d47a6fe53d418ba2a909ae1
Paolalopez4 <00167523@uca.edu.sv>
2026-05-08 16:46:59 -0600

    feat: creacion de paquetes principales y configuracion en yaml

---
+++ el_sistema_de_los_7_mares/src/main/java/org/lab2ejercicio/el_sistema_de_los_7_mares/ElSistemaDeLos7MaresApplication.java
@@ -0,0 +1,13 @@
+package org.lab2ejercicio.el_sistema_de_los_7_mares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElSistemaDeLos7MaresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElSistemaDeLos7MaresApplication.class, args);
+    }
+
+}
---
+++ el_sistema_de_los_7_mares/src/test/java/org/lab2ejercicio/el_sistema_de_los_7_mares/ElSistemaDeLos7MaresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lab2ejercicio.el_sistema_de_los_7_mares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElSistemaDeLos7MaresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
